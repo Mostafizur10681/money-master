5dd91ca5a60b0cc5c5f963cdd3b9ef515955ffa1
Mostafizur rahman <sourav10681@gmail.com>
2022-02-17 03:17:12 +0600

    error message added

--- js/app.js
+++ js/app.js
@@ -15,33 +15,53 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const rentInput = getInputValue('rent');
     const clothesInput = getInputValue('clothes');
 
-    const expenses = document.getElementById('expenses-total');
-    const expensesValue = expenses.innerText;
-    const expensesTotal = foodInput + rentInput + clothesInput;
-    expenses.innerText = expensesTotal;
-    const balance = document.getElementById('balance-total');
-    const balanceValue = balance.innerText;
-    const balanceTotal = incomeInput - expensesTotal;
-    balance.innerText = balanceTotal;
+    if (incomeInput < 0 || isNaN(incomeInput) || incomeInput == null) {
+        alert('Please Enter a positive Intiger number')
+
+    }
+    else if (foodInput < 0 || isNaN(foodInput) || foodInput == null) {
+        alert('Please Enter a Positive Intiger value of food')
+    }
+    else if (rentInput < 0 || isNaN(rentInput) || rentInput == null) {
+        alert('Please Enter a Positive Intiger value of rent')
+    }
+    else if (clothesInput < 0 || isNaN(clothesInput) || clothesInput == null) {
+        alert('Please Enter a Positive Intiger value of clothes')
+    }
+    else {
+        const expenses = document.getElementById('expenses-total');
+        const expensesValue = expenses.innerText;
+        const expensesTotal = foodInput + rentInput + clothesInput;
+        expenses.innerText = expensesTotal;
+        const balance = document.getElementById('balance-total');
+        const balanceValue = balance.innerText;
+        const balanceTotal = incomeInput - expensesTotal;
+        balance.innerText = balanceTotal;
+    }
+
+
 
 })
 
 document.getElementById('save-button').addEventListener('click', function () {
 
     const incomeInput = getInputValue('income');
-    const savingInput = document.getElementById('saving-input');
-    const savingInputValue = parseFloat(savingInput.value)
-    const saveParcentage = savingInputValue / 100;
-    const newSavingAmount = incomeInput * saveParcentage;
+    const savingInputValue = getInputValue('saving-input')
+    if (incomeInput < 0 || isNaN(incomeInput) || incomeInput == null) {
+        alert('Please Enter a positive Intiger number')
+    }
+    else {
 
+        const saveParcentage = savingInputValue / 100;
+        const newSavingAmount = incomeInput * saveParcentage;
+        const savingAmount = document.getElementById('saving-amount');
+        savingAmount.innerText = newSavingAmount;
 
-    const savingAmount = document.getElementById('saving-amount');
-    savingAmount.innerText = newSavingAmount;
-
-    const remaining = document.getElementById('remaining-balance');
-    const balance = document.getElementById('balance-total').innerText;
-    const remainingAmount = balance - newSavingAmount;
-    remaining.innerText = remainingAmount
+        const remaining = document.getElementById('remaining-balance');
+        const balance = document.getElementById('balance-total').innerText;
+        const remainingAmount = balance - newSavingAmount;
+        remaining.innerText = remainingAmount;
+    }
 
 
 })
